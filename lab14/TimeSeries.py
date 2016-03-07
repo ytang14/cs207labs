ee4da1f7126df7ae5a65d1a1dae9dee5117672a8
ytang14 <tang01@g.harvard.edu>
2016-03-07 15:25:11 -0500

    added commend line

--- lab14/TimeSeries.py
+++ lab14/TimeSeries.py
@@ -1,6 +1,5 @@
 import numpy as np
 from lazy import *
-
 class TimeSeries():
     '''
     """
@@ -78,7 +77,15 @@ DESCRIPTION
     @lazy
     def lazy(self):
         return self
-
+    def itervalues(self):
+        for v in self._values:
+            yield v
+    def itertimes(self):
+        for t in self._times:
+            yield t
+    def iteritemes(self):
+        for t,v in zip(self._times,self._values):
+            yield (t,v)
     def __len__(self):
         return len(self._TimeSeries[0])
     
